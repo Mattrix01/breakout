e3db1ed49924e5fe92c6f1698c5808293f932377
Mattrix01 <emjay.knight@gmail.com>
2022-12-09 15:22:16 +0000

    ball bouncing off wall, collision+block issues

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
 const blockWidth = 100;
 const blockHeight = 20;
+const ballDiameter = 20;
 const boardWidth = 560;
+const boardHeight = 300;
+let xDirection = -2;
+let yDirection = 2;
 
 // where user block starts
 const userStart = [230, 10];
@@ -11,6 +16,9 @@ let currentPosition = userStart;
 const ballStart = [270, 40];
 let ballCurrentPosition = ballStart;
 
+let timerId;
+let score = 0;
+
 // create block
 // passing through 2 values to figure out where our block goes starting form the left.
 class Block {
@@ -18,13 +26,13 @@ class Block {
     // bottom left the anchor point
     this.bottomLeft = [xAxis, yAxis];
     this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
     this.topLeft = [xAxis, yAxis + blockHeight];
-    this.topRight = [xAxis + blockHeight, yAxis + blockHeight];
   }
 }
+
 //all my blocks, how ever many we add we create, 15 below.
 const blocks = [
-  new Block(10, 270),
   new Block(10, 270),
   new Block(120, 270),
   new Block(230, 270),
@@ -43,7 +51,7 @@ const blocks = [
 ];
 
 // drawing all blocks
-function addBLocks() {
+function addBlocks() {
   for (let i = 0; i < blocks.length; i++) {
     // created a div and stored as block
     const block = document.createElement("div");
@@ -53,16 +61,47 @@ function addBLocks() {
     block.style.left = blocks[i].bottomLeft[0] + "px";
     block.style.bottom = blocks[i].bottomLeft[1] + "px";
     grid.appendChild(block);
+    console.log(blocks[i].bottomLeft);
   }
 }
-
-addBLocks();
 
 //add user
 const user = document.createElement("div");
 user.classList.add("user");
-drawUser();
 grid.appendChild(user);
+drawUser();
+
+//add ball
+const ball = document.createElement("div");
+ball.classList.add("ball");
+grid.appendChild(ball);
+drawBall();
+
+//move user
+function moveUser(e) {
+  // listening to an event of keys of arrow left or right.
+  switch (e.key) {
+    // if value of arrow left we move current position below
+    case "ArrowLeft":
+      // if statement to stop from going off screen
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        console.log(currentPosition[0] > 0);
+        drawUser();
+      }
+      break;
+    case "ArrowRight":
+      // if statement to stop from going off screen
+      if (currentPosition[0] < boardWidth - blockWidth) {
+        currentPosition[0] += 10;
+        console.log(currentPosition[0]);
+        drawUser();
+      }
+      break;
+  }
+}
+// everytime hear a mouse down, invoke moveUser function, then see if key arrow left etc
+document.addEventListener("keydown", moveUser);
 
 //draw the user in function because using the positioning often
 function drawUser() {
@@ -76,44 +115,80 @@ function drawBall() {
   ball.style.bottom = ballCurrentPosition[1] + "px";
 }
 
-//move user
-function moveUser(e) {
-  // listening to an event of keys of arrow left or right.
-  switch (e.key) {
-    // if value of arrow left we move current position below
-    case "ArrowLeft":
-      // if statement to stop from going off screen
-      if (currentPosition[0] > 0) {
-        currentPosition[0] -= 10;
-        drawUser();
+//move ball
+function moveBall() {
+  ballCurrentPosition[0] += xDirection;
+  ballCurrentPosition[1] += yDirection;
+  drawBall();
+  checkForCollisions();
+}
+timerId = setInterval(moveBall, 30);
+
+//check for collisions
+function checkForCollisions() {
+  //check for block collision
+  for (let i = 0; i < blocks.length; i++) {
+    if (
+      ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+      ballCurrentPosition[0] < blocks[i].bottomRight[0] &&
+      ballCurrentPosition[1] + ballDiameter > blocks[i].bottomLeft[1] &&
+      ballCurrentPosition[1] < blocks[i].topLeft[1]
+    ) {
+      const allBlocks = Array.from(document.querySelectorAll(".block"));
+      allBlocks[i].classList.remove("block");
+      blocks.splice(i, 1);
+      changeDirection();
+      score++;
+      scoreDisplay.innerHTML = score;
+      if (blocks.length == 0) {
+        scoreDisplay.innerHTML = "You Win!";
+        clearInterval(timerId);
+        document.removeEventListener("keydown", moveUser);
       }
-      break;
-    case "ArrowRight":
-      // if statement to stop from going off screen
-      if (currentPosition[0] < boardWidth - blockWidth) {
-        currentPosition[0] += 10;
-        drawUser();
-      }
-      break;
+    }
+  }
+  // check for wall hits
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[0] <= 0 ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter
+  ) {
+    changeDirection();
+  }
+
+  //check for user collision
+  if (
+    ballCurrentPosition[0] > currentPosition[0] &&
+    ballCurrentPosition[0] < currentPosition[0] + blockWidth &&
+    ballCurrentPosition[1] > currentPosition[1] &&
+    ballCurrentPosition[1] < currentPosition[1] + blockHeight
+  ) {
+    changeDirection();
+  }
+
+  //game over check
+  if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId);
+    scoreDisplay.innerHTML = "You lose!";
+    document.removeEventListener("keydown", moveUser);
   }
 }
 
-// everytime hear a mouse down, invoke moveUser function, then see if key arrow left etc
-document.addEventListener("keydown", moveUser);
-
-//add ball
-const ball = document.createElement("div");
-ball.classList.add("ball");
-drawBall();
-// putting ball inside the parent with appendChild
-grid.appendChild(ball);
-
-//moving ball
-function moveBall() {
-  ballCurrentPosition[0] += 2;
-  ballCurrentPosition[1] += 2;
-  drawBall();
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2;
+    return;
+  }
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2;
+    return;
+  }
 }
-
-// every 30ms move ball
-setInterval(moveBall, 30);
